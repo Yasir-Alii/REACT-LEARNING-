02fb308c8ada09e32339f1db31cb0e49e60edf8c
Yasir-Alii <binjabbar10@gmail.com>
2024-03-14 15:31:42 -0700

    Added some new features

--- react-learning/src/main.jsx
+++ react-learning/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Header from './assets/Components/Header.jsx'
 
 
 
@@ -8,6 +9,7 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Header/>
     
 
   </React.StrictMode>,
