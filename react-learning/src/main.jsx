f8455d4c977bbc3b73d7332a13a88d463bfca118
Yasir-Alii <binjabbar10@gmail.com>
2024-03-16 17:17:01 +0500

    Some Changes

--- react-learning/src/main.jsx
+++ react-learning/src/main.jsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Header from './assets/Components/Header.jsx'
 
-
+// function MyApp() {
+//   return(
+//     <div>
+//       <h3>THIS IS JUST <FUNCTION></FUNCTION></h3>
+//     </div>
+//   )
+// }
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App/>
     <Header/>
+  
     
 
   </React.StrictMode>,
